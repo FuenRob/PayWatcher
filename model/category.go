adbfe44f56c10220be62b21e8baa55c37fe07821
Roberto Morais <fuenrob@gmail.com>
2024-06-20 19:17:13 +0200

    fix: Add UserID field to Category and Payment models

--- model/category.go
+++ model/category.go
@@ -2,6 +2,7 @@ package model
 
 type Category struct {
 	ID        uint   `json:"id" gorm:"primaryKey"`
+	UserID    uint   `json:"user_id"`
 	Name      string `json:"name"`
 	Priority  uint   `json:"priority"`
 	Recurrent bool   `json:"recurrent"`
