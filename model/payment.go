c7797fc0d8684a248cc47e0d1c8b3d2bfc2a67fd
Roberto Morais <fuenrob@gmail.com>
2024-07-11 19:32:49 +0200

    chore: Add payment routes and controller methods for CRUD operations

--- model/payment.go
+++ model/payment.go
@@ -1,10 +1,24 @@
 package model
 
+import "time"
+
 type Payment struct {
-	ID          uint    `json:"id" gorm:"primaryKey"`
+	ID          uint      `json:"id" gorm:"primaryKey"`
+	Name        string    `json:"name"`
+	CategoryID  uint      `json:"category_id"`
+	UserID      uint      `json:"user_id"`
+	NetAmount   float64   `json:"net_amount"`
+	GrossAmount float64   `json:"gross_amount"`
+	Deductible  float64   `json:"deductible"`
+	ChargeDate  time.Time `json:"charge_date"`
+	Recurrent   bool      `json:"recurrent"`
+	PaymentType string    `json:"payment_type"`
+	Paid        bool      `json:"paid"`
+}
+
+type UpdateOrCreatePayment struct {
 	Name        string  `json:"name"`
 	CategoryID  uint    `json:"category_id"`
-	UserID      uint    `json:"user_id"`
 	NetAmount   float64 `json:"net_amount"`
 	GrossAmount float64 `json:"gross_amount"`
 	Deductible  float64 `json:"deductible"`
