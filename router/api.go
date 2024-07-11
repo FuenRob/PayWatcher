ea0dc6f034804b6af7fe2463a2b21a41bc2007a1
Roberto Morais <fuenrob@gmail.com>
2024-07-11 19:32:49 +0200

    chore: Add payment routes and controller methods for CRUD operations

--- router/api.go
+++ router/api.go
@@ -26,4 +26,12 @@ func Init(app *fiber.App) {
 	category.Get("/:id", middleware.ProtectedHandler(), controller.GetCatories)
 	category.Put("/:id", middleware.ProtectedHandler(), controller.UpdateCategory)
 	category.Delete("/:id", middleware.ProtectedHandler(), controller.DeleteCategory)
+
+	payment := api.Group("/payment")
+	payment.Post("/", middleware.ProtectedHandler(), controller.CreatePayment)
+	payment.Get("/", middleware.ProtectedHandler(), controller.GetPayment)
+	payment.Get("/:id", middleware.ProtectedHandler(), controller.GetPayment)
+	payment.Get("/category/:idCategory", middleware.ProtectedHandler(), controller.GetPaymentsByCategoryID)
+	payment.Put("/:id", middleware.ProtectedHandler(), controller.UpdatePayment)
+	payment.Delete("/:id", middleware.ProtectedHandler(), controller.DeletePayment)
 }
