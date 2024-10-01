3f72c2db3c37af92d6eddbfb864701cf980a5f0c
Roberto Morais <fuenrob@gmail.com>
2024-10-01 18:20:46 +0200

    Add default Cors

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"PayWatcher/router"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	go cronjob.InitCronJobs()
 	database.Connect()
 	app := fiber.New()
+	app.Use(cors.New())
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
